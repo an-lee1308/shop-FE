23cc6a3febdff36ff67ae000d36effe427a919ac
an.lee1308 <an.lee1308@example.com>
2022-02-25 22:34:46 +0700

    config axios

---
+++ src/config/axiosInstance.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const AxiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_URL,
+  headers: {
+    "content-type": "application/json",
+  },
+});
+
+AxiosInstance.interceptors.response.use(
+  (response) => {
+    if (response && response.data) {
+      return response.data;
+    }
+    return response;
+  },
+  (err) => {
+    throw err;
+  }
+);
+export default AxiosInstance;
